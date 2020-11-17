0ab4ce230278a92cce008a6423d7b985445fd2fa
Baudouin Raoult <baudouin.raoult@ecmwf.int>
2020-11-17 16:53:34 +0000

    Testing

--- tests/test_mars.py
+++ tests/test_mars.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 import ecmwfapi
+import time
+
 
 def test_mars():
-   c = ecmwfapi.ECMWFService("mars")
-   c.execute({"date": "2000-01-01"}, "mars.grib")
+    while True:
+        try:
+            c = ecmwfapi.ECMWFService("mars")
+            c.execute({"date": "2000-01-01"}, "mars.grib")
+            return
+        except ecmwfapi.api.APIException as e:
+            print(e)
+            msg = str(e)
+            if "USER_QUEUED_LIMIT_EXCEEDED" not in msg:
+                raise
+
+        time.sleep(120)
+
 
 if __name__ == "__main__":
     test_mars()
