dd20383585359cc80249826977ff102c3a81deee
cristian-codorean <cristian.codorean@gmail.com>
2022-01-31 11:19:30 +0000

    Bump up version, for release.

--- ecmwfapi/api.py
+++ ecmwfapi/api.py
@@ -44,7 +44,7 @@ except ImportError:
     sys.exit("Python socket module was not compiled with SSL support. Aborting...")
 
 
-VERSION = "1.6.2"
+VERSION = "1.6.3"
 
 
 DEFAULT_RCFILE_PATH = "~/.ecmwfapirc"
